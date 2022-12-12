cc4b690557e7003476bf45f811340538b8b312f0
ECGregersen <117631068+ECGregersen@users.noreply.github.com>
2022-12-12 11:01:12 +0100

    changes

--- SemesterProjekt/src/model/OrderLine.java
+++ SemesterProjekt/src/model/OrderLine.java
@@ -2,8 +2,13 @@ package model;
 
 public class OrderLine {
 	private int amount;
+	private AbstractProduct abstractProduct;
+	private Order order;
 	
-	
+	public OrderLine(AbstractProduct abstractProduct) {
+		this.abstractProduct = abstractProduct;
+	}
+
 	public void increaseQtyByOne() {
 		this.amount++;
 	}
