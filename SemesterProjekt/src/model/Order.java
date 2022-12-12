6ba83edea9ca80294c6d9b990426eb7d7f73d34e
ECGregersen <117631068+ECGregersen@users.noreply.github.com>
2022-12-12 10:32:17 +0100

    Order

--- SemesterProjekt/src/model/Order.java
+++ SemesterProjekt/src/model/Order.java
@@ -1,5 +1,23 @@
 package model;
 
+import java.time.LocalDate;
+
 public class Order {
+	private int orderId;
+	private double totalPrice;
+	private boolean condition;
+	private String deliveryLocation;
+	private LocalDate date;
+	
+	public Order() {
+	}
+
+	public void setDeliveryLocation(String deliveryLocation) {
+			this.deliveryLocation = deliveryLocation;
+	}
+		
+	public void setTotalPrice(double totalPrice) {
+			this.totalPrice = totalPrice;
+	}
 
 }
--- SemesterProjekt/src/model/OrderLine.java
+++ SemesterProjekt/src/model/OrderLine.java
@@ -1,5 +1,6 @@
 package model;
 
 public class OrderLine {
-
+	private int amount;
+	
 }
