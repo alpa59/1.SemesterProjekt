a8270e6570a1f1c079b1a45b395011efe6eb154a
UCNDanny <117714228+UCNDanny@users.noreply.github.com>
2022-12-12 10:40:58 +0100

    Update OrderLine

--- SemesterProjekt/src/model/OrderLine.java
+++ SemesterProjekt/src/model/OrderLine.java
@@ -3,4 +3,13 @@ package model;
 public class OrderLine {
 	private int amount;
 	
+	
+	public void increaseQtyByOne() {
+		this.amount++;
+	}
+	
+	public int getAmount() {
+		return amount;
+	}
+	
 }
