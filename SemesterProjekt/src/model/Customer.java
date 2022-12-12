2ef15fb8a1d6ec99388de38a0bcbe82902e6563d
tiseksogfyrre <10461241@ucn.dk>
2022-12-12 10:51:01 +0100

    Customer

--- SemesterProjekt/src/model/Customer.java
+++ SemesterProjekt/src/model/Customer.java
@@ -2,4 +2,24 @@ package model;
 
 public class Customer {
 
+	private String customerID;
+	private String name;
+	private String email;
+	private String phoneNumber;
+	private String address;
+	private int discount;
+	private int credit;
+	private pinCode pinCode;
+	
+	
+	
+	
+	public String getPhoneNumber() {
+		return phoneNumber;
+	}
+	public void setPhoneNumber(String phoneNumber) {
+		this.phoneNumber = phoneNumber;
+	}
+	
+	
 }
