991a2bb3b0970fd6411e88d6aa44b24c0ffeb7a9
UCNDanny <117714228+UCNDanny@users.noreply.github.com>
2022-12-12 10:30:22 +0100

    Update Personel

--- SemesterProjekt/src/model/Personel.java
+++ SemesterProjekt/src/model/Personel.java
@@ -1,5 +1,17 @@
 package model;
 
 public class Personel {
+	private String name;
+	private String phoneNumber;
+	private String personelID;
+	private String email;
+	private String title;
 	
+	public Personel(String name, String phoneNumber, String personelID, String email, String title) {
+		this.name = name;
+		this.phoneNumber = phoneNumber;
+		this.personelID = personelID;
+		this.email = email;
+		this.title = title;
+	}
 }
