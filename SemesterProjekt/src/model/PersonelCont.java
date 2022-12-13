2665fd3433f50e2581871bf78abc6e7548bea47a
alpa59 <117630853+alpa59@users.noreply.github.com>
2022-12-13 11:38:15 +0100

    PersonelColum

--- SemesterProjekt/src/model/PersonelCont.java
+++ SemesterProjekt/src/model/PersonelCont.java
@@ -6,4 +6,20 @@ public class PersonelCont {
 
 	private static PersonelCont instance;
 	private ArrayList<Personel> personels;
+
+	public static PersonelCont getInstance() {
+		if (instance == null) {
+			instance = new PersonelCont();
+		}
+		return instance;
+	}
+
+	private PersonelCont() {
+		personels = new ArrayList<>();
+	}
+
+	public void addPersonel(Personel personel) {
+		personels.add(personel);
+	}
+	
 }
